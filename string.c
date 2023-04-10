e1f5254a704e737cacec2d0753abc2ec0ee880f0
John Muthua <caveomuthama@gmail.com>
2023-04-10 10:42:29 +0300

    Added another portion to string

--- string.c
+++ string.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <cstring>
 
 int main(void)
 {
-    char s1[] = "my string";
-    char* s2 = "my string";
+    char s1[30] = "my string";
+
+    strcpy(s1, "Another string");
 
     printf("%s", s1);
-    printf("%s \n", s2[4]);
     printf("\n The end of the for loop");
 }
