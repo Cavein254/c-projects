84de88425ca4e5d5185293d32943bf5260a7e080
johnmuthua <caveomuthama@gmail.com>
2023-06-15 16:44:05 +0300

    Add another file

--- char/compare.c
+++ char/compare.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-void cmps(const char*, const char*);
+int cmps(const char*, const char*);
 int main () 
 {
     const char * s1 = "This is a string";
-    const char * s2 = "This is a string";
+    const char * s2 = "This is a string ";
     cmps(s1, s2);
 }
 
-void cmps (const char *s1, const char *s2)
+int cmps (const char *s1, const char *s2)
 {
     while (*s1 != '\n') {
         if (*s1 != *s2){
             printf("The two strings are not equal \n");
             return (1);
         }
+        s1++;s2++;
     }
-    printf("This strings are equal");
+    printf("This strings are equal \n");
     return (0);
 }
